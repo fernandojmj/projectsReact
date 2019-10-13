a9b22b4e06dcc656de11a4a530c4b4cc6cdcacf2
Fernando <fernandojmj@gmail.com>
2019-10-13 01:24:21 -0300

    commit relacionado ao meetweb

---
+++ meetWeb/meetweb/src/routes/Route.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { PropTypes } from "prop-types";
+import { Route, Redirect } from "react-router-dom";
+import AuthLayout from "~/pages/_layouts/auth";
+import DefaultLayout from "~/pages/_layouts/default";
+
+export default function RouteWrapper({
+  component: Component,
+  isPrivate,
+  ...rest
+}) {
+  const signed = true;
+
+  if (!signed && isPrivate) {
+    return <Redirect to="/"></Redirect>;
+  }
+
+  if (signed && !isPrivate) {
+    return <Redirect to="/dashboard"></Redirect>;
+  }
+
+  const Layout = signed ? DefaultLayout : AuthLayout;
+
+  return (
+    <Route
+      {...rest}
+      render={props => (
+        <Layout>
+          <Component {...props}></Component>
+        </Layout>
+      )}
+    />
+  );
+}
+
+RouteWrapper.propTypes = {
+  isPrivate: PropTypes.bool,
+  component: PropTypes.oneOfType([PropTypes.element, PropTypes.func]).isRequired
+};
+
+RouteWrapper.defaultProps = {
+  isPrivate: false
+};
